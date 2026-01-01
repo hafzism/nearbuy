8664c2724f314ebaacb09ac34eee92533a31c5a8
hafzism <hafeezpallath@gmail.com>
2026-01-01 17:03:10 +0530

    Fix: Force media serving (images) in production mode

--- backend/product_finder/urls.py
+++ backend/product_finder/urls.py
@@ -14,17 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import path, include
-
-from product_finder import settings
-
-from django.views.generic import RedirectView
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('product_finder/',include('product_app.urls')),
-    path('', RedirectView.as_view(url='/product_finder/login'))
+    path('', RedirectView.as_view(url='/product_finder/login')),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
-urlpatterns=urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
